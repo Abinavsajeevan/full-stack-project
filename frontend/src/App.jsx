82aceb1f0b99689879efc2a4858b9deba3a84f69
Abinavsajeevan <abinavabi2k02@gmail.com>
2026-04-09 22:27:20 +0530

    frontend navigationworks

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './index.css'
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
---
+++ frontend/src/places/pages/NewPlace.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPLace = () => {
+    return <h1>New Place work!</h1>
+}
+
+export default NewPLace
